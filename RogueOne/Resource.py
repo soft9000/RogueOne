c023f0470e0bd228d8be4b424294db762ae0615d
Soft9000.com <r.a.nagy@gmail.com>
2019-03-23 18:51:23 -0400

    Delete Resource.py

--- RogueOne/Resource.py
+++
@@ -1,9 +0,0 @@
-
-from RogueOne.Shower import Shower
-
-class PlayerResource(Shower):
-
-    def __init__(self, name, glyph, xpos, ypos):
-        super().__init__(name, glyph, xpos, ypos)
-        
-
