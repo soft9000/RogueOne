24e551b253758a9e3bff88243e55d50b7d6fd4bf
Soft9000.com <r.a.nagy@gmail.com>
2019-03-25 06:07:39 -0400

    Added rodent-click support.
    Simplified the design. Minimalist is the goal?

--- MainOne/RogueOne/Console.py
+++ MainOne/RogueOne/Console.py
@@ -4,17 +4,64 @@ from RogueOne.View import View
 
 PLAYER = "Player"
 
+class Event:
+    ''' Add a key-level event into the loop - see set_KeyEvent(below) '''
+    def __init__(self, keyChar, helpStr, on_call):
+        self.keyChar = keyChar
+        self.helpStr = helpStr
+        self.on_call = on_call
+
+
 class ConsoleApplication(Rectangle):   
     '''
     The READILY visible aspect / viewport over your
     simulation (i.e. actual area / map could be
     A LOT larger!)
     '''
-    def __init__(self):
+    def __init__(self, char_up='e', char_down='c', char_left='s', char_right='f'):
         super().__init__(80,25)
+        self.game_over  = False
+        self.parent = None
         self.movers  = dict()
         self.markers = dict()
         self.resources = dict()
+        self.actions    = dict()
+        self.set_KeyEvent(Event(char_up, "up", self.do_up))
+        self.set_KeyEvent(Event(char_down, "down", self.do_down))
+        self.set_KeyEvent(Event(char_left, "left", self.do_left))
+        self.set_KeyEvent(Event(char_right, "right", self.do_right))
+        self.set_KeyEvent(Event('?', "help", self.do_help))
+        self.set_KeyEvent(Event('!', "quit", self.do_quit))
+
+    def set_Parent(self, parent):
+        self.parent = parent
+
+    def set_KeyEvent(self, event):
+        ''' Support user-assignable, single-key, events '''
+        if isinstance(event, Event) is False:
+            return False
+        self.actions[event.keyChar] = event
+        return True
+
+    def get_KeyEvents(self):
+        return list(self.actions.keys())
+
+    def event_MouseClick(self, button, xpos, ypos):
+        print(button, xpos, ypos)
+
+    def event_KeyPress(self, event):
+        key = event.char
+        if key in self.actions:
+            action = self.actions[key].on_call
+            if action:
+                action()
+        if not self.game_over:
+            self.event_gameStep()
+
+    def event_gameStep(self):
+        if self.do_next() is False:
+            self.game_over = True
+        self.on_plot()
 
     def add_mover(self, mover):
         ''' Add a console Mover '''
@@ -46,40 +93,49 @@ class ConsoleApplication(Rectangle):
             return self.movers[PLAYER]
         return None
     
-    def do_up(self, view):
+    def do_up(self):
         player = self._get_player()
         if player:
             player.on_move(player.location[0], player.location[1] - 1)
 
-    def do_down(self, view):
+    def do_down(self):
         player = self._get_player()
         if player:
             player.on_move(player.location[0], player.location[1] + 1)
 
-    def do_left(self, view):
+    def do_left(self):
         player = self._get_player()
         if player:
             player.on_move(player.location[0] - 1, player.location[1])
 
-    def do_right(self, view):
+    def do_right(self):
         player = self._get_player()
         if player:
             player.on_move(player.location[0] + 1, player.location[1])
-        
-    def on_plot(self, view):
+
+    def do_help(self):
+        for key in self.actions:
+            info = self.actions[key]
+            print("'{}' = {}".format(info.keyChar, info.helpStr))
+ 
+    def do_quit(self):
+        self.game_over = True
+        self.parent.stop()
+       
+    def on_plot(self):
         '''
         Draw the screen. Will be called after the game is over
         (i.e. do_next() returns False) so as to allow us to
         display gameplay results (etc.)
         '''
-        view.clear_screen()
+        self.parent.clear_screen()
         # Givers FIRST
         for key in self.resources:
-            self.resources[key].on_plot(view)
+            self.resources[key].on_plot(self.parent)
         for key in self.markers:
-            self.markers[key].on_plot(view)
+            self.markers[key].on_plot(self.parent)
         # Takers LAST
         for key in self.movers:
-            self.movers[key].on_plot(view)
-        view.show_screen()    
+            self.movers[key].on_plot(self.parent)
+        self.parent.show_screen()    
 
--- MainOne/RogueOne/Events.py
+++ MainOne/RogueOne/Events.py
@@ -2,21 +2,13 @@
 
 from collections import OrderedDict
 from tkinter import *
-
-
-class Event:
-    ''' Add a key-level event into the loop - see set_KeyEvent(below) '''
-    def __init__(self, keyChar, helpStr, on_call):
-        self.keyChar = keyChar
-        self.helpStr = helpStr
-        self.on_call = on_call
         
 
-class EventLoop(Tk):
+class EventGUI(Tk):
     
     ''' The TK window that we will use for drawing, as well as managing input events. '''
 
-    def __init__(self, char_up='e', char_down='c', char_left='s', char_right='f', width=800, height=600):
+    def __init__(self, width=800, height=600):
         super().__init__()
         self.font_sz    = 16
         self.width      = width
@@ -28,32 +20,22 @@ class EventLoop(Tk):
         self.view       = None
         self.canvas     = None
         self.font       = None
-        self.game_over  = False
-        self.actions    = dict()
-        self.set_KeyEvent(Event(char_up, "up", self.on_up))
-        self.set_KeyEvent(Event(char_down, "down", self.on_down))
-        self.set_KeyEvent(Event(char_left, "left", self.on_left))
-        self.set_KeyEvent(Event(char_right, "right", self.on_right))
-        self.set_KeyEvent(Event('?', "help", self.on_help))
-        self.set_KeyEvent(Event('!', "quit", self.on_quit))
 
-    def set_KeyEvent(self, event):
-        ''' Support user-assignable, single-key, events '''
-        if isinstance(event, Event) is False:
-            return False
-        self.actions[event.keyChar] = event
-        return True
-
-    def get_KeyEvents(self):
-        return list(self.actions.keys())
-
-    def start(self):
+    def start(self, view):
+        self.view = view
         self.canvas = Canvas(self, width=self.width, height=self.height)
         self.bind_all('<KeyPress>', self.event_KeyPress, '+')
+        self.bind_all('<Button-1>', self.event_MouseClick, '+')
+        self.bind_all('<Button-2>', self.event_MouseClick, '+')
+        self.bind_all('<Button-3>', self.event_MouseClick, '+')
         self.canvas.pack()
         self.clear_screen()
         self.font = ("Courier", self.font_sz, "bold")
+        self.view.set_Parent(self)
         super().mainloop()
+
+    def stop(self):
+        self.destroy()
 
     def clear_screen(self):
         ''' Fills the internal screen-buffer with the default character. '''
@@ -89,50 +71,13 @@ class EventLoop(Tk):
         for key in zdict:
             print("{0:>10s} = {1} ".format(key, zdict[key]))
         print('=' * 10)
-
-    def event_KeyPress(self, event):
-        if False:
-            self.event_Dump(event)
-        else:
-            key = event.char
-            if key in self.actions:
-                action = self.actions[key].on_call
-                if action:
-                    action(self)
-            if self.game_over:
-                self.destroy()
-            else:
-                self.event_gameStep()
-
-    def event_gameStep(self):
-        if self.view.do_next() is False:
-            self.game_over = True
-        self.view.on_plot(self)
-
-    def on_up(self, view):
-        self.view.do_up(self)
-
-    def on_down(self, view):
-        self.view.do_down(self)
-
-    def on_left(self, view):
-        self.view.do_left(self)
-
-    def on_right(self, view):
-        self.view.do_right(self)
-
-    def on_help(self, view):
-        for key in self.actions:
-            info = self.actions[key]
-            print("'{}' = {}".format(info.keyChar, info.helpStr))
-
-    def on_quit(self, view):
-        self.game_over = True
-
-    def on_start(self, view):
-        self.view = view
-        self.start()
     
+    def event_MouseClick(self, event):
+        self.view.event_MouseClick(event.num, event.x_root, event.y_root)
+    
+    def event_KeyPress(self, event):
+        self.view.event_KeyPress(event)
+        
     def mainloop(self, view):
-        self.on_start(view)
+        self.start(view)
 
